87b3be67288dda21aae11487b47000b451a850d9
Vladyslav Boiko <v.boiko@bumpapp.xyz>
2023-04-04 16:31:52 +0300

    TODO

--- app/src/main/java/com/example/anothertestapp/MainActivity.kt
+++ app/src/main/java/com/example/anothertestapp/MainActivity.kt
@@ -11,6 +11,19 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * TODO list:
+     * - Took a large amount of time to catch BOOT event, still working not good
+     * - Need to check and request permissions, push notifications won't work on API33 +
+     * - I dont have any presenters/viewmodels ets (sort of architecture in this project)
+     * - Some logic not moved to the right places
+     * -Alarm schedules in onCreate of activity (i don't remember if in this implementation it replace the old alarm event),
+     *   we need to properly handle this event, because it schedules every time when we launch app
+     * - DI
+     * - Incorrect handling of data for push notifications (didn't have enough time for this)
+     * Ps. no gitignore :)
+     */
+
     private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
